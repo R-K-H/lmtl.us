de462aeb82ebd2fdbfbec639e909e1b775ca13b2
Kollan House <kollanh@gmail.com>
2018-08-18 01:07:38 -0700

    added in checks for fields.

--- register.php
+++ register.php
@@ -11,13 +11,13 @@ Failing to sanitize can lead to cross-site scripting vulnerabilities.
 */
 
 if (isset($_POST)){
-	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB);
+	$mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB);
 
-	if (!$link) {
+	if (!$mysqli) {
 	    exit;
 	}
 	$stmt = $mysqli->prepare("INSERT INTO connection ('name','email','phone','type','contact') VALUES (?,?,?,?,?)");
-	$stmt->bind_param("sssii", $_POST['name'], $_POST['email'], $_POST['phone'], $_POST['type'], $_POST['contact']);
+	$stmt->bind_param("sssib", $_POST['name'], $_POST['email'], $_POST['phone'], $_POST['type'], $_POST['contact']);
 	$stmt->execute();
 	$stmt->close();
 
